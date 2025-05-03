7a03a388218fd57a4297f9c76d8bba740d3d7411
Sabi Kahlon <sabikahlon7345@gmail.com>
2025-05-03 19:28:55 -0400

    about page updated

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,13 @@
 
 import AboutMain from "../components/About/AboutMain"
+import WorkWithMe from "../components/WorkWithMe"
+import Footer from "../components/Footer"
 export default function About() {
   return (
     <div>
       <AboutMain />
+      <WorkWithMe />
+      <Footer />
     </div>
   )
 }
