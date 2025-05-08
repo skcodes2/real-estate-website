e7c16583cdff905d2e61552d0c8c763ec377b3ad
Sabi Kahlon <sabikahlon7345@gmail.com>
2025-05-08 15:28:24 -0400

    mini contact section done

---
+++ src/components/Contact/MiniContact.tsx
@@ -0,0 +1,38 @@
+import "./MiniContact.css";
+import pic from "../../assets/KuldipPic4.png";
+import { useNavigate } from "react-router-dom";
+
+export default function MiniContact() {
+    const navigate = useNavigate();
+  return (
+    <section className="mini-contact-container bg-color">
+      <div className="mini-contact-image-wrapper">
+        <img src={pic} alt="Kuldip" className="mini-contact-image" />
+      </div>
+
+      <div className="mini-contact-content">
+        <div className="mini-name title-md">Kuldip Kahlon</div>
+        <div className="mini-divider"></div>
+
+        <div className="mini-info">
+          <div className="mini-info-section">
+            <p className="mini-info-label body-text">Title</p>
+            <p className="mini-info-value body-text">Real Estate Agent</p>
+          </div>
+          <div className="mini-info-section">
+            <p className="mini-info-label body-text">Email</p>
+            <p className="mini-info-value body-text">kuldipsandhu1@hotmail.com</p>
+          </div>
+          <div className="mini-info-section">
+            <p className="mini-info-label body-text">Phone Number</p>
+            <p className="mini-info-value body-text">414 605 4058</p>
+          </div>
+        </div>
+
+        <div className="mini-button-wrapper">
+          <button className="mini-button button2 " onClick={()=>navigate("/contact")}>Contact Me</button>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/PropertyDetails.tsx
+++ src/pages/PropertyDetails.tsx
@@ -2,6 +2,7 @@ import PropertyImageSlider from "../components/Properties/PropertyImageSlider"
 import PropertySummary from "../components/Properties/PropertySummary"
 import PropertyFeatures from "../components/Properties/PropertyFeatures"
 import { useParams } from "react-router-dom"
+import MiniContact from "../components/Contact/MiniContact"
 export default function PropertyDetails() {
     const { address } = useParams<{ address: string }>()
   return (
@@ -9,6 +10,8 @@ export default function PropertyDetails() {
         {address && <PropertyImageSlider address={address}/>}
         {address && <PropertySummary/>}
           {address && <PropertyFeatures />}
+          {address && <MiniContact />}
+
     </section>
   )
 }
