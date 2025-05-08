ee532884889f4f797943ccfef1cc96cb8cb07282
Sabi Kahlon <sabikahlon7345@gmail.com>
2025-05-07 22:06:59 -0400

    updated home properties slider to now fetch from database

---
+++ src/components/Properties/PropertyImageSlider.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { PropertyDetails } from './PropertyTypes';
+import { getProperties } from './PropertiesMain';
+
+import 'swiper/swiper-bundle.css';
+// FreeMode styles are included in the main Swiper CSS
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+import "./PropertyDetails.css"
+
+
+import { Autoplay, Navigation} from 'swiper/modules';
+
+export default function PropertyImageSlider({address}: { address: string }) {
+  const [loading, setLoading] = useState(true);
+  const [property, setProperty] = useState<PropertyDetails>();
+
+  useEffect(() => {
+    getProperties()
+      .then((data) => {
+        if (data.length > 0) {
+            console.log("Fetched properties:", data);
+            const foundProperty = data.find((prop:PropertyDetails) => prop.address === address);
+            console.log("Found property:", foundProperty);
+          setProperty(foundProperty); // Use the first property for the slider
+        }
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching properties:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div className="slider-loading">Loading images...</div>;
+  }
+
+  if (!property || !property.images || property.images.length === 0) {
+    return <div className="slider-loading">No images available.</div>;
+  }
+
+  return (
+    <div className="property-details-slider-container bg-color">
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 244500,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
+        modules={[Autoplay, Navigation]}
+        
+        scrollbar={true}
+        className="mySwiper2"
+      >
+        {property.images.map((url, idx) => (
+          <SwiperSlide key={`main-${idx}`}>
+            <div className="property-image-wrapper">
+            <img src={url} alt={`Property image ${idx + 1}`} />
+            </div>
+            
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      </div>
+  );
+}
--- src/pages/Properties.tsx
+++ src/pages/Properties.tsx
@@ -1,11 +1,15 @@
 
 import PropertiesIntro from "../components/Properties/PropertiesIntro"
 import PropertiesMain from "../components/Properties/PropertiesMain"
+import WorkWithMe from "../components/WorkWithMe"
+import Footer from "../components/Footer"
 export default function Properties() {
   return (
     <div>
         <PropertiesIntro/>
       <PropertiesMain />
+      <WorkWithMe />
+      <Footer />
     </div>
   )
 }
